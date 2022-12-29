1cf5813d8bd18510a5c0f424efe4ce4718409fdd
Xiangyu Liu <neulxy@gmail.com>
2022-12-29 16:53:28 +1300

    showing a loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,6 +12,7 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   apiURL =
     'https://ng-complete-guide-3d267-default-rtdb.firebaseio.com/posts.json';
+  isFetching = false;
 
   constructor(private http: HttpClient) {}
 
@@ -38,6 +39,7 @@ export class AppComponent implements OnInit {
   }
 
   private fetchPosts() {
+    this.isFetching = true;
     this.http
       .get<{ [key: string]: Post }>(this.apiURL)
       .pipe(
@@ -52,6 +54,7 @@ export class AppComponent implements OnInit {
         })
       )
       .subscribe((posts) => {
+        this.isFetching = false;
         this.loadedPosts = posts;
       });
   }
